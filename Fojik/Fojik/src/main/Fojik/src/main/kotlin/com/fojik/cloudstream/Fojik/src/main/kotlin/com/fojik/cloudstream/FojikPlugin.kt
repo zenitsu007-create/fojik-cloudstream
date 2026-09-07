42b923f3249a6e06b523153c05fc1ca37d7cbd55
zenitsu007-create <explorerssquad2@gmail.com>
2026-09-07 20:48:55 +0600

    Create FojikPlugin.kt

---
+++ Fojik/Fojik/src/main/Fojik/src/main/kotlin/com/fojik/cloudstream/Fojik/src/main/kotlin/com/fojik/cloudstream/FojikPlugin.kt
@@ -0,0 +1,11 @@
+package com.fojik.cloudstream
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class FojikPlugin : Plugin() {
+    override fun load(context: android.content.Context) {
+        registerMainAPI(FojikProvider())
+    }
+}
