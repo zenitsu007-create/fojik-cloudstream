2b2606cea0ad5a8c661ed5dfca75c65f5af195de
zenitsu007-create <explorerssquad2@gmail.com>
2026-09-08 00:35:00 +0600

    Update FojikPlugin.kt

--- Fojik/Fojik/src/main/Fojik/src/main/kotlin/com/fojik/cloudstream/Fojik/src/main/kotlin/com/fojik/cloudstream/Fojik/src/main/kotlin/com/fojik/cloudstream/FojikPlugin.kt
+++ Fojik/Fojik/src/main/Fojik/src/main/kotlin/com/fojik/cloudstream/Fojik/src/main/kotlin/com/fojik/cloudstream/Fojik/src/main/kotlin/com/fojik/cloudstream/FojikPlugin.kt
@@ -3,8 +3,4 @@ package com.fojik.cloudstream
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
 @CloudstreamPlugin
-class FojikPlugin {
-    fun load() {
-        FojikProvider()
-    }
-}
+class FojikPlugin
