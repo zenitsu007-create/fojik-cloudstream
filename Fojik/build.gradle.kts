b383dac6d37870fa97c0b0b57f47c963eae711e8
zenitsu007-create <explorerssquad2@gmail.com>
2026-09-07 20:41:58 +0600

    Create build.gradle.kts

---
+++ Fojik/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    description = "Fojik movie metadata provider"
+    authors = listOf("FojikCloudStream")
+    status = 1
+    tvTypes = listOf("Movie")
+    language = "en"
+    iconUrl = "https://fojik.site/wp-content/uploads/new-mlw-bd-red.jpg"
+}
