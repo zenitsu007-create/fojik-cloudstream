b60e8ae4766bd94183d512ad2eddde1a34f9677e
zenitsu007-create <explorerssquad2@gmail.com>
2026-09-07 22:47:01 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,47 +1,42 @@
+import com.android.build.gradle.BaseExtension
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
 buildscript {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
+        maven("https://jitpack.io")
     }
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     }
 }
 
-subprojects {
-    apply(plugin = "com.android.library")
-    apply(plugin = "org.jetbrains.kotlin.android")
-    apply(plugin = "com.lagradost.cloudstream3.gradle")
-
+allprojects {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-
-    dependencies {
-        val cloudstream by configurations
-        cloudstream("com.lagradost:cloudstream3:pre-release")
-        implementation("org.jsoup:jsoup:1.18.3")
-    }
-
-    android {
-        compileSdk = 35
-        namespace = "com.fojik.cloudstream"
-        defaultConfig {
-            minSdk = 21
-        }
-    }
-
-    kotlin {
-        jvmToolchain(17)
-    }
-
-    cloudstream {
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "YOUR_GITHUB_USERNAME/FojikCloudStream")
+        maven("https://jitpack.io")
     }
 }
+
+fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
+    extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+
+fun Project.android(configuration: BaseExtension.() -> Unit) =
+    extensions.getByName<BaseExtension>("android").configuration()
+
+subprojects {
+    apply(plugin = "com.android.library")
+    apply(plugin = "kotlin-android")
+    apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+    cloudstream {
+        setRepo(
+            System.getenv("GITHUB_REPOSITORY")
+                ?: "https://github.com/user/repo
